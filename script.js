b7eb8053d46fceca1a8857c70378c77816ccb47f
Hari <harikrishnan53828@gmail.com>
2022-03-22 22:27:31 +0530

    tt

---
+++ script.js
@@ -0,0 +1,20 @@
+function display(value)
+{
+     document.getElementById("result").value += value;
+}
+function clearScreen(value)
+{
+     document.getElementById("result").value = "";
+}
+function calculate()
+{
+     var a = document.getElementById("result").value;
+     var b = eval(a);
+     document.getElementById("result").value= b;
+}
+
+function del(value)
+{
+     document.getElementById("result").value +=value.slice(0 , -1);
+
+}
