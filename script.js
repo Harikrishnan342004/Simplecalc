2eff549d3f69a99cf27dcf4b4e2ed9b4d2d1a66c
Hari <harikrishnan53828@gmail.com>
2023-08-05 15:42:45 +0530

    del

---
+++ script.js
@@ -0,0 +1,27 @@
+function display(value)
+{
+     document.getElementById("result").value += value;
+}
+
+
+function clearScreen(value)
+{
+     document.getElementById("result").value = "";
+}
+
+
+function calculate()
+{
+     var a = document.getElementById("result").value;
+     var b = eval(a);
+     document.getElementById("result").value= b;
+}
+
+function del()
+{
+    
+     result.value = result.value.slice(0, -1);
+      
+
+}
+
